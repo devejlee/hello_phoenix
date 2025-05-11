36e99f1b5df973829dd35f0c2049403d0cba2131
devejlee <54133475+devejlee@users.noreply.github.com>
2025-05-11 17:31:11 +0900

    play around with ecto

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -3,10 +3,18 @@ defmodule HelloWeb.HelloController do
   plug :put_view, html: HelloWeb.HelloHTML, json: HelloWeb.PageJSON
   # plug :put_view, "html: HelloWeb.PageHTML", json: HelloWeb.PageJSON
 
+  # def index(conn, _params) do
+  #   render(conn, :index)
+  # end
+
+  # use admin.html.heex layout
   def index(conn, _params) do
-    render(conn, :index)
+    conn
+    |> put_layout(html: :admin)
+    |> render(:index)
   end
 
+  @spec show(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def show(conn, %{"messenger" => messenger}) do
     render(conn, :show, messenger: messenger)
   end
